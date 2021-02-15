4facdbc325211caa18673cb31dbb1a048ff8aa41
hibeen1 <hibeen1@gmail.com>
2021-02-16 02:04:49 +0900

    First Commiting my API

---
+++ moreabangebbangyabbangya.py
@@ -0,0 +1,74 @@
+from bs4 import BeautifulSoup
+import json
+import requests
+
+
+def moreabangebbangyabbangya(queryName):
+    html = requests.get(
+        'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + queryName)
+    soup = BeautifulSoup(html.text, 'html.parser')
+
+    #검색어를 잘 입력한 경우
+    try:
+        apartment_Information = dict()
+        thisApartment = dict()
+        #최근 매매 실거래 정보 parsing
+        res = soup.findAll('div', {'class': 'info_area'})[1]
+        recent_trading_price = res.find('span', {'class': 'item2'}).text
+        recent_trading_info = res.find(
+            'span', {'class': 'item2'}).next_sibling.next.text
+        #json에 저장
+        thisApartment["recent trade information"] = recent_trading_info + " " + \
+            recent_trading_price
+
+        acreage_num = soup.findAll('span', {'class': 'tab_inner'})
+        info = soup.findAll('div', {'class': 'complex_content'})
+        for i in range(0, len(acreage_num)):
+            detail = dict()
+            #해당 면적
+            acreage = acreage_num[i].text
+            detail["해당 면적"] = acreage
+            #매물가격 1
+            tem = info[i].findAll('div', {'class': 'detail'})[
+                1].find('li', {'class': 'item'})
+            subject_1 = tem.find(
+                'strong', {'class': 'price'}).previous_sibling
+            price_of_subject_1 = tem.find('strong', {'class': 'price'}).text
+            detail[subject_1] = price_of_subject_1
+
+            try:
+                #매물가격 2 (없을 수도 있음)
+                c1 = tem.next_sibling.next
+                subject_2 = c1.find(
+                    'strong', {'class': 'price'}).previous_sibling
+                price_of_subject_2 = c1.find('strong', {'class': 'price'}).text
+                detail[subject_2] = price_of_subject_2
+            except:
+                
+                detail[""] = "다른 매물이 없습니다"
+            
+            #매물 정보(없을 수도 있음)
+            try:
+                tem2 = info[i].find('div', {'class': 'menu_area'}).text
+                detail["매물 수량"] = tem2
+            except:
+                detail["매물 수량"]: "정보가 없습니다"
+            thisApartment[acreage] = detail
+        apartment_Information[queryName] = thisApartment
+
+
+        with open('../test.json', 'w', encoding='UTF-8') as make_file:
+            json.dump(apartment_Information, make_file, indent="\t")
+
+        
+        # 저장한 파일 출력하기
+        with open('../test.json', 'r') as f:
+            json_data = json.load(f)
+            #한글 인코딩 안 될 때 ensure_ascii=False
+            print(json.dumps(json_data, ensure_ascii=False, indent="\t"))
+    #검색어를 잘못 입력한 경우
+    except:
+        print("알맞은 검색어를 입력하세요")
+
+moreabangebbangyabbangya("벽산비치타운")
+
